671e67127c89c48513770f9756b4cbdf28d4e762
omerkocaman <omr_kcmn@outlook.com>
2024-06-07 15:27:25 +0300

    init

---
+++ Barcodeiki/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Barcodeiki;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Barcodeiki/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using BarcodeScanning;
+
+namespace Barcodeiki;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	
+
+    async void CameraView_OnDetectionFinished(System.Object sender, BarcodeScanning.OnDetectionFinishedEventArg e)
+    {
+        var result = e.BarcodeResults;
+		string okunanlar = "";
+		if (result.Count() > 0 )
+		{
+            foreach (var item in result)
+            {
+                okunanlar += " " + item.BarcodeFormat + " " + item.DisplayValue + "\n";
+            }
+
+            await DisplayAlert("Barkod", okunanlar, "TAmam");
+        }
+		
+        
+        await Task.Delay(1500);
+    }
+}
+
